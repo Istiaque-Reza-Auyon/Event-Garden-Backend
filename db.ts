76c7bd93e1f778176732e878f685045ef5f5ba03
unknown <auyoncep16@gmail.com>
2024-06-30 21:03:18 +0600

    added database name in env

--- db.ts
+++ db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize('Event Garden', process.env.DB_DIALECT!, process.env.DB_PASS, {
+const db = new Sequelize(process.env.DB_NAME!, process.env.DB_DIALECT!, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT as Dialect,
     port: Number(process.env.DB_PORT)
