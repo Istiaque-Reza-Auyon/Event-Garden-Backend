b03a58d23df79f944984405e322c15a8962a9fc8
unknown <auyoncep16@gmail.com>
2024-07-01 18:40:23 +0600

    fixed token issues

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -6,10 +6,9 @@ import { findUserByIdQuery } from '../model/user/query';
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const secretKey = process.env.SECRET_KEY!;
     if (req.headers.token) {
-        console.log(req.headers.token);
         jwt.verify(req.headers.token as string, secretKey, (err: any, decoded: any) => {
             if (err) {
-                console.log(err)
+                console.error(err)
                 res.json('error parsing jwt')
             } else {
                 req.body.user = decoded;
